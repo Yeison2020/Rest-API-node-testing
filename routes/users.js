2c302b68a32593e2f8345451065a360736908346
Yeison <enmanuelquiterios@gmail.com>
2022-02-03 23:56:19 -0500

    added delete

--- routes/users.js
+++ routes/users.js
@@ -2,26 +2,9 @@ import express from "express";
 import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
-const users = [
-  {
-    name: "Enmanuel",
-    lastname: "casado",
-    age: 26,
-  },
-  {
-    name: "Yeuri",
-    lastname: "casado",
-    age: 23,
-  },
-
-  {
-    name: "Deivi",
-    lastname: "casado",
-    age: 22,
-  },
-];
+let users = [];
 router.get("/", (req, res) => {
-  console.log(users);
+  // console.log(users);
   res.send(users);
 });
 
@@ -33,9 +16,23 @@ router.post("/", (req, res) => {
   const userWithId = { ...user, id: uuidv4() };
   users.push(userWithId);
   res.send(`User with the name of ${user.name} added to the database`);
-  console.log(userWithId);
+  // console.log(userWithId);
 });
 
-// router.get("/id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  // How to access Params from URL
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  // Why users = users This setting my current database = to It's new deleted value mutuating database deleteing the Element with the const {id} = req.body
+  // Revisar porque el filter deletes The values here
+  users = users.filter((user) => user.id !== id);
+  res.send(users);
+});
 
 export default router;
