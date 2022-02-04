608d5005caea548078097016013165524cfdc232
Yeison <enmanuelquiterios@gmail.com>
2022-02-03 22:57:09 -0500

    adding users

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import usersRouter from "./routes/users.js";
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.get("/", (rep, resp) => {
+app.get("/", (req, resp) => {
   console.log("test");
   resp.send("Welcome to home page");
 });
+// Here is how I set my staring routes check changing the values.
+
+// 1. the routes would be "users/users" app.use("users/+ usersRouter = users")
+app.use("/users", usersRouter);
 
 app.listen(PORT, () =>
   console.log(`Server Running on port: http://localhost:${PORT}`)
