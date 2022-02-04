ff4d342eaec4f998cf95298e5124210b87ab4922
Yeison <enmanuelquiterios@gmail.com>
2022-02-03 22:39:23 -0500

    Adding resquests

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
-// They both the same
-// const express = require("express");
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+
+app.get("/", (rep, resp) => {
+  console.log("test");
+  resp.send("Welcome to home page");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server Running on port: http://localhost:${PORT}`)
+);
